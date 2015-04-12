37a79c71893935c58dd11e915c778bb62f0c7206
Peter Scott <peter_scott@brown.edu>
2015-04-12 12:12:03 -0400

    added bubble sort functionality

--- Sorting/src/Sorting/BubbleSort.java
+++ Sorting/src/Sorting/BubbleSort.java
@@ -8,7 +8,18 @@ public class BubbleSort {
 	 */
 	public static int[] bubbleSort(int[] unsortedArray) {
 		//TODO: S1 implements bubble sort
-		
-		return null;
+		int length = unsortedArray.length;
+
+		for (int i = 0; i < (length - 1); i++) {
+			for (int j = 0; j < (length - i - 1); j++) {
+				if (unsortedArray[j] > unsortedArray[j+1]) {
+					int swap = unsortedArray[j];
+					unsortedArray[j] = unsortedArray[j+1];
+					unsortedArray[j+1] = swap;
+				}
+			}
+		}
+			
+		return unsortedArray;
 	}
 }
--- Sorting/src/Sorting/Main.java
+++ Sorting/src/Sorting/Main.java
@@ -22,8 +22,9 @@ public class Main {
 		
 		System.out.println("Sorted Array:");
 		//TODO: sort the array and print out its contents
-		
-		
+		int[] sortedArray = BubbleSort.bubbleSort(unsortedArray);
+		for (int i: sortedArray) {
+			System.out.print(i + " ");
+		}
 	}
-
 }
